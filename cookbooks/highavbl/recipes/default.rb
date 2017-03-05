61d13ec8ee1f6a321782ad0574f5303506bc3c63
srana <srana@frndz.com>
2017-03-05 23:42:15 +0530

    Modified the file to read the nodes via search instead of hardcoding

--- cookbooks/highavbl/recipes/default.rb
+++ cookbooks/highavbl/recipes/default.rb
@@ -3,17 +3,21 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
-node.default['haproxy']['members'] = [{
-    'hostname' => 'web1', # these values come from the Vagrantfile
-    'ipaddress' => '192.168.10.43', # or the public IP and hostname is using a cloud provider
-    'port' => 80,
-    'ssl_port' => 80
-    },{
-    'hostname' => 'web2', # these values come from the Vagrantfile
-    'ipaddress' => '192.168.10.44', # or the public IP and hostname is using a cloud provider
-    'port' => 80,
-    'ssl_port' => 80
-    }
-]
+all_web_nodes = search('node',"role:web")
 
-include_recipe 'haproxy::manual'
+members = []
+
+all_web_nodes.each do |web_node|
+
+member = {
+    	'hostname'  => web_node['hostname'],
+  	'ipaddress' => web_node['ipaddress'],
+  	'port' => 80,
+  	'ssl_port' => 80
+	}
+members.push(member)
+
+end
+
+node.default['haproxy']['members'] = members
+include_recipe "haproxy::manual"
