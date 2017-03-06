7d07777711316e4460d68621362cfb5961160072
srana <srana@frndz.com>
2017-03-07 00:32:56 +0530

    Added more contents and info

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -4,8 +4,13 @@
 #
 # @see https://docs.chef.io/resource_package.html
 #
-package 'httpd'
+package 'httpd' do 
+	action:install
+end
 
+remote_file '/var/www/html/ironman.png' do 
+	source 'https://www.sideshowtoy.com/photo.php?sku=902622'
+end
 #
 # The template, named '/var/www/html/index.html' is created
 #   with the source 'index.html.erb'
@@ -19,8 +24,12 @@ package 'httpd'
 #
 
 template '/var/www/html/index.html' do
-  source 'index.html.erb'
+	source 'index.html.erb'
+	variables(:name=>'My Apache Server')
+	action :create
+	notifies :restart, 'service[httpd]', :immediately
 end
+
 
 #
 # The service, named "httpd" is enabled and started
@@ -28,5 +37,11 @@ end
 # @see https://docs.chef.io/resource_service.html
 
 service 'httpd' do
-  action [:enable, :start]
+	action [ :enable, :start ]
 end
+
+
+
+
+
+
